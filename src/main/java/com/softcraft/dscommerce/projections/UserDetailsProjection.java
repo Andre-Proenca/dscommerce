8003c1136e838a479883baa8499e5204785ff090
Andre Proenca <andreproenca@hotmail.com>
2025-01-10 13:50:44 -0400

    UserDetailsService

---
+++ src/main/java/com/softcraft/dscommerce/projections/UserDetailsProjection.java
@@ -0,0 +1,9 @@
+package com.softcraft.dscommerce.projections;
+
+public interface UserDetailsProjection {
+
+    String getUsername();
+    String getPassword();
+    Long getRoleId();
+    String getAuthority();
+}
