974f2100210e87bbb67e55cfed1806cbb5ed9d67
Andre Proenca <andreproenca@hotmail.com>
2025-01-14 19:25:52 -0400

    Saving Order

--- src/main/java/com/softcraft/dscommerce/dto/OrderDTO.java
+++ src/main/java/com/softcraft/dscommerce/dto/OrderDTO.java
@@ -8,6 +8,8 @@ import com.softcraft.dscommerce.entities.Order;
 import com.softcraft.dscommerce.entities.OrderItem;
 import com.softcraft.dscommerce.entities.OrderStatus;
 
+import jakarta.validation.constraints.NotEmpty;
+
 public class OrderDTO {
 	
 	private Long id;
@@ -18,6 +20,7 @@ public class OrderDTO {
 	
 	private PaymentDTO payment;
 	
+	@NotEmpty(message = "Deve ter ao menos um item")
 	private List<OrderItemDTO> items = new ArrayList<>();
 
 	public OrderDTO(Long id, Instant moment, OrderStatus status, ClientDTO client, PaymentDTO payment) {
--- src/main/java/com/softcraft/dscommerce/services/OrderService.java
+++ src/main/java/com/softcraft/dscommerce/services/OrderService.java
@@ -1,12 +1,21 @@
 package com.softcraft.dscommerce.services;
 
+import java.time.Instant;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.softcraft.dscommerce.dto.OrderDTO;
+import com.softcraft.dscommerce.dto.OrderItemDTO;
 import com.softcraft.dscommerce.entities.Order;
+import com.softcraft.dscommerce.entities.OrderItem;
+import com.softcraft.dscommerce.entities.OrderStatus;
+import com.softcraft.dscommerce.entities.Product;
+import com.softcraft.dscommerce.entities.User;
+import com.softcraft.dscommerce.repositories.OrderItemRepository;
 import com.softcraft.dscommerce.repositories.OrderRepository;
+import com.softcraft.dscommerce.repositories.ProductRepository;
 import com.softcraft.dscommerce.services.exceptions.ResourceNotFoundException;
 
 @Service
@@ -15,11 +24,44 @@ public class OrderService {
 	@Autowired
 	private OrderRepository repository;
 	
+	
+	@Autowired
+	private ProductRepository productRepository;
+	
+	@Autowired
+	private OrderItemRepository orderItemRepository;
+	
+	@Autowired
+	private UserService userService;
+	
 	@Transactional(readOnly = true)
     public OrderDTO findById(Long id) {
             Order order = repository.findById(id).orElseThrow(
                     () -> new ResourceNotFoundException("RECURSO NAO ENCONTRADO!"));
             return new OrderDTO(order);
     }
+
+	@Transactional
+	public OrderDTO insert(OrderDTO dto) {
+		
+		Order order = new Order();
+		
+		order.setMoment(Instant.now());
+		order.setStatus(OrderStatus.WAITING_PAYMENT);
+		
+		User user = userService.authenticated();
+		order.setClient(user);
+		
+		for (OrderItemDTO itemDto : dto.getItems()) {
+			Product product = productRepository.getReferenceById(itemDto.getProductId());
+			OrderItem item = new OrderItem(order, product, itemDto.getQuantity(), product.getPrice());
+			order.getItems().add(item);
+		}
+		
+		repository.save(order);
+		orderItemRepository.saveAll(order.getItems());
+		
+		return new OrderDTO(order);
+	}
 	
 }
