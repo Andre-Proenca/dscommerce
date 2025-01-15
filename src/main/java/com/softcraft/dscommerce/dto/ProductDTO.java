87ddae33cc021d4433a6e4024b2532312e1123bb
Andre Proenca <andreproenca@hotmail.com>
2025-01-15 12:23:29 -0400

    Fix validation

--- src/main/java/com/softcraft/dscommerce/dto/ProductDTO.java
+++ src/main/java/com/softcraft/dscommerce/dto/ProductDTO.java
@@ -8,6 +8,7 @@ import com.softcraft.dscommerce.entities.Product;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
 import jakarta.validation.constraints.Size;
 
@@ -20,9 +21,11 @@ public class ProductDTO {
     @Size(min = 10, message = "Descrição precisa ter no mínimo 10 caracteres")
     @NotBlank(message = "Campo requerido")
     private String description;
+    @NotNull(message = "Campo requerido")
     @Positive(message = "Somente valores positivos")
     private Double price;
     private String imgUrl;
+    
     @NotEmpty(message = "Deve conter ao menos uma categoria")
     private List<CategoryDTO> categories = new ArrayList<>();
 
